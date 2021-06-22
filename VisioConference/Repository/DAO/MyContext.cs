1a9e815c8cb40575416185d652af9cd2e823800a
Martin Taquet <sunitram@hotmail.fr>
2021-06-22 18:26:54 +0200

    Premiers test de deploiement

---
+++ VisioConference/Repository/DAO/MyContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using VisioConference.Repository.Objets;
+
+namespace VisioConference.Repository.DAO
+{
+    public class MyContext : DbContext
+    {
+
+        public MyContext()
+            : base("name=MyContext")
+        {
+        }
+
+        public virtual DbSet<User> users { get; set; }
+        public virtual DbSet<Conversation> conversations { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+            .Property(e => e.login)
+            .IsUnicode(false);
+
+            modelBuilder.Entity<Conversation>()
+          .Property(e => e.userID)
+          .IsUnicode(false);
+
+            modelBuilder.Entity<Conversation>()
+               .Property(e => e.userFriendID)
+            .IsUnicode(false);
+
+
+
+
+        }
+    }
+
+}
---
+++ VisioConference/Repository/Objets/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace VisioConference.Repository.Objets
+{
+    public class User
+    {
+        public User()
+        {
+            conversations = new HashSet<Conversation>();
+        }
+        [Key]
+        public int id { get; set; }
+        [StringLength(255)]
+        [Index(IsUnique = true)]
+        [Required]
+        public string login { get; set; }
+
+        //Indique dans la BD si l'utilisateur est en connexion ou pas - lorsqu'il ferme l'app, il met la valeur à false
+        public bool connected { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Conversation> conversations { get; set; }
+    }
+}
