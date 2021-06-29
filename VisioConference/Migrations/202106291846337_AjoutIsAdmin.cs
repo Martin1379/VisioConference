3db6386e28492f6d6f1d58723e8e0188dfed953b
Martin Taquet <sunitram@hotmail.fr>
2021-06-29 20:56:33 +0200

    Rajout IsAdmin, change l'emplacement du DTO et rajoute le convertisseur

---
+++ VisioConference/Migrations/202106291846337_AjoutIsAdmin.cs
@@ -0,0 +1,18 @@
+﻿namespace VisioConference.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AjoutIsAdmin : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Users", "IsAdmin", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Users", "IsAdmin");
+        }
+    }
+}
---
+++ VisioConference/Tools/Convertisseur.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VisioConference.DTO;
+using VisioConference.Repository.Objets;
+
+namespace VisioConference.Tools
+{
+    public class Convertisseur
+    {
+        public static UserDTO UserDTOFromUser(UserDTO dto, User model)
+        {
+            dto.Id = model.Id;
+            dto.Login = model.Login;
+            dto.Password = model.Password;
+            dto.Email = model.Email;
+            dto.Photo = model.Photo;
+            dto.Connected = model.Connected;
+            dto.IsAdmin = model.IsAdmin;
+
+            return dto;
+        }
+
+        public static User UserFromUserDTO(UserDTO dto, User model)
+        {
+            model.Id = dto.Id;
+            model.Login = dto.Login;
+            model.Password = dto.Password;
+            model.Email = dto.Email;
+            model.Photo = dto.Photo;
+            model.Connected = dto.Connected;
+            model.IsAdmin = dto.IsAdmin;
+
+            return model;
+        }
+    }
+
+}
