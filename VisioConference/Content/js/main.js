a8085e7482167e621572711cb6bd99694c55f7d8
Your Name <you@example.com>
2021-07-18 21:48:00 +0200

    RemyFrontModifierProfil

--- VisioConference/Content/js/main.js
+++ VisioConference/Content/js/main.js
@@ -1,6 +1,35 @@
-﻿//ONGLET CONTATCS
+﻿
+//BOUTON MODIFIER PROFIL
+btnModifProfil();
+
+function btnModifProfil() {
+    var x = document.getElementById("modifier-profil-content");
+    var y = document.getElementById("btn-modif-profil");
+   
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+//BOUTON ANNULER MODIFICATION PROFIL
+function btnAnnulerProfil() {
+    var x = document.getElementById("modifier-profil-content");
+    x.style.display = "none";
+
+    var y = document.getElementById("btn-modif-profil");
+    y.style.display = "block";
+
+}
+
+//ONGLET CONTATCS
 document.getElementById("contacts-tab").onclick = function () {
     document.getElementById("contacts-tab").className = "nav-tabs active";
+    document.getElementById("iuserplus").className = "fas fa-user-plus";
+    document.getElementById("iusers").className = "fas fa-users iblue";
     document.getElementById("invitations-tab").className = "nav-tabs";
     document.getElementById("contacts-content").className = "d-in";
     document.getElementById("invitations-content").className = "d-no";
@@ -9,6 +38,8 @@ document.getElementById("contacts-tab").onclick = function () {
 //ONGLET INVITATIONS
 document.getElementById("invitations-tab").onclick = function () {
     document.getElementById("invitations-tab").className = "nav-tabs active";
+    document.getElementById("iusers").className = "fas fa-users";
+    document.getElementById("iuserplus").className = "fas fa-user-plus iblue";
     document.getElementById("contacts-tab").className = "nav-tabs";
     document.getElementById("invitations-content").className = "d-in";
     document.getElementById("contacts-content").className = "d-no";
