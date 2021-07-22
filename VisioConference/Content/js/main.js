e040f6d5b6d09fdcdf7508bc4dcd203c891ba4da
Martin Taquet <sunitram@hotmail.fr>
2021-07-22 13:33:27 +0200

    Debug

--- VisioConference/Content/js/main.js
+++ VisioConference/Content/js/main.js
@@ -1,4 +1,4 @@
-﻿//BOUTON MODIFIER PROFIL
+﻿/*BOUTON MODIFIER PROFIL*/
 btnModifProfil();
 
 function btnModifProfil() {
@@ -45,30 +45,30 @@ document.getElementById("invitations-tab").onclick = function () {
 }
 
 //INPUT ENVOYER MESSAGE
-$(function () {
-    console.log("Passage par signaLR");
-    // Declare a proxy to reference the hub.
-    var chat = $.connection.chatHub;
-    // Create a function that the hub can call to broadcast messages.
-    chat.client.broadcastMessage = function (name, message) {
-        // Html encode display name and message.
-        var encodedName = $('<div />').text(name).html();
-        var encodedMsg = $('<div />').text(message).html();
-        // Add the message to the page.
-        $('#discussion').append('<li><strong>' + encodedName
-            + ':&nbsp;</strong>' + encodedMsg + '</li><br />');
-    };
-    // Get the user name and store it to prepend to messages.
-    /*$('#displayname').val(prompt('Enter your name:', ''));*/
-    // Set initial focus to message input box.
-    $('#message').focus();
-    // Start the connection.
-    $.connection.hub.start().done(function () {
-        $('#sendmessage').click(function () {
-            // Call the Send method on the hub.
-            chat.server.send($('#displayname').val(), $('#message').val());
-            //// Clear text box and reset focus for next comment.
-            //$('#message').val('').focus();
-        });
-    });
-});
+//$(function () {
+//    console.log("Passage par signaLR");
+//    // Declare a proxy to reference the hub.
+//    var chat = $.connection.chatHub;
+//    // Create a function that the hub can call to broadcast messages.
+//    chat.client.broadcastMessage = function (name, message) {
+//        // Html encode display name and message.
+//        var encodedName = $('<div />').text(name).html();
+//        var encodedMsg = $('<div />').text(message).html();
+//        // Add the message to the page.
+//        $('#discussion').append('<li><strong>' + encodedName
+//            + ':&nbsp;</strong>' + encodedMsg + '</li><br />');
+//    };
+//    // Get the user name and store it to prepend to messages.
+//    /*$('#displayname').val(prompt('Enter your name:', ''));*/
+//    // Set initial focus to message input box.
+//    $('#message').focus();
+//    // Start the connection.
+//    $.connection.hub.start().done(function () {
+//        $('#sendmessage').click(function () {
+//            // Call the Send method on the hub.
+//            chat.server.send($('#displayname').val(), $('#message').val());
+//            //// Clear text box and reset focus for next comment.
+//            //$('#message').val('').focus();
+//        });
+//    });
+//});
